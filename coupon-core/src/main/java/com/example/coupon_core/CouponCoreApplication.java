e1c3a8f5c0685995a29d140206ef817b47528324
postwo <codo7717@naver.com>
2025-09-23 03:13:05 +0900

    멀티 모듈  ì기본 설정

---
+++ coupon-core/src/main/java/com/example/coupon_core/CouponCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.coupon_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponCoreApplication.class, args);
+	}
+
+}
