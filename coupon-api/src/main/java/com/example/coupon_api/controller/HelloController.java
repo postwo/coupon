b036266c3df0d4108ffc8be931512d77630bea72
postwo <codo7717@naver.com>
2025-09-23 15:13:53 +0900

    부하 테스트

--- coupon-api/src/main/java/com/example/coupon_api/controller/HelloController.java
+++ coupon-api/src/main/java/com/example/coupon_api/controller/HelloController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello() throws InterruptedException {
+        Thread.sleep(500);
         return "hello";
-    }
+    } // rps = 초당 2건 처리 * n(서버에서 동시에 처리할 수 있는 수) ex) 2*200 =400 얼추 비슷하게 나온다
+    // thread pool 에서 요청을 처리하는데 톰캣에서 사용하는 max thread pool 은 기본적으로 200 개이다 그러므로 n은 200을 넣는다
+    // yml에서 tread max 사이즈를 늘리면 그거에 맞게 변한다 ex) 2 * 400 = 800
 }
