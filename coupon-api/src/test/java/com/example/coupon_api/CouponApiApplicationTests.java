b4a48bee1c7ee46b42f2cd722dc46cb31c75d039
postwo <codo7717@naver.com>
2025-10-03 23:50:04 +0900

    # 메인 coupon 에서 build 실패 실패 이유는 redis 환경 변수값들이 test 에 적용이 안되었기 떄문에 해결 방안은 test 에다가 각각 yml 설정을 해주면 해결

--- coupon-api/src/test/java/com/example/coupon_api/CouponApiApplicationTests.java
+++ coupon-api/src/test/java/com/example/coupon_api/CouponApiApplicationTests.java
@@ -2,7 +2,14 @@ package com.example.coupon_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
+// ActiveProfiles,TestPropertySource 를 달아준 이유는
+// core에 있는 redis 환경 변수 값들을 못가지고 오기 떄문에 사용
+// 이렇게 하면 test 에서 발생 하는 에러는 해결
+@ActiveProfiles("test")
+@TestPropertySource(properties = "spring.config.name=application-test")
 @SpringBootTest
 class CouponApiApplicationTests {
 
